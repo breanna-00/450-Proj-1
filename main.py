cf98da915551cc4486f8b35e9934ca697ae5f613
githelsui <githelsuico@gmail.com>
2022-10-04 20:55:40 -0700

    main.py

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from houserep import getArrTwitterHandles, choosePeople
 import numpy as np
 import pandas as pd
 
-# Get user inputs
+# Get user inputs @nikki
 chosenMembers = choosePeople()
 TwitterAPI = authTwitterAPI()
 columns = ['User', 'Tweets']
 rawTweets = []
+
+# Get raw tweet data @githel @breanna
 for member in chosenMembers:
     handle = member[2]
     tweets = getTweets(handle,TwitterAPI) 
@@ -16,3 +18,9 @@ for member in chosenMembers:
 rawTweets_df = pd.DataFrame(rawTweets, columns=columns)
 
 print(rawTweets_df)
+
+# Clean raw tweet data @katrina
+
+# use rawTweets_df 
+
+# Send clean tweet data to Natural Language API @githel
